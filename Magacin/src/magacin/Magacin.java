6f9f535e71c6b749564ca37fe0490990a5d647cc
goran9999 <gutovicgoran@gmail.com>
2022-03-13 18:32:33 +0100

    Implementirane metode u klasi Magacin na grani master.

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,36 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import interfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs{
 
+	private List<Artikal> artikli=new LinkedList<>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
-		
+		if(artikal!=null) {
+			artikli.add(0, artikal);
+		}else {
+			throw new IllegalArgumentException("Artikal ne moze biti null!");
+		}
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		artikli.remove(artikal);
 		
 	}
 
 	@Override
 	public Artikal pretraziArtikal(int sifraArtikla) {
-		// TODO Auto-generated method stub
+		for (Artikal artikal : artikli) {
+			if(artikal.getSifra()==sifraArtikla) {
+				return artikal;
+			}
+		}
 		return null;
 	}
 	
