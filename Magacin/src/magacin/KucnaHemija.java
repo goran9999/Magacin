1866485c2ba44236873809133fa0b798cbe2921e
goran9999 <gutovicgoran@gmail.com>
2022-03-13 17:51:56 +0100

    Dodate equals,hashCode i toString metode

--- Magacin/src/magacin/KucnaHemija.java
+++ Magacin/src/magacin/KucnaHemija.java
@@ -3,8 +3,34 @@ package magacin;
 import java.util.Date;
 
 public class KucnaHemija {
-
+	
 	private Date rokTrajanja;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((rokTrajanja == null) ? 0 : rokTrajanja.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		if (rokTrajanja == null) {
+			if (other.rokTrajanja != null)
+				return false;
+		} else if (!rokTrajanja.equals(other.rokTrajanja))
+			return false;
+		return true;
+	}
+
 
 	public Date getRokTrajanja() {
 		return rokTrajanja;
@@ -12,6 +38,11 @@ public class KucnaHemija {
 
 	public void setRokTrajanja(Date rokTrajanja) {
 		this.rokTrajanja = rokTrajanja;
+	}
+
+	@Override
+	public String toString() {
+		return "KucnaHemija [rokTrajanja=" + rokTrajanja + "]";
 	}
 	
 	
