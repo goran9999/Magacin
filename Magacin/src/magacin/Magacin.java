f25101cdd15f51834962ef6ea559a4e766df0859
goran9999 <gutovicgoran@gmail.com>
2022-03-13 18:19:50 +0100

    Dodata klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,26 @@
+package magacin;
+
+import interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs{
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pretraziArtikal(int sifraArtikla) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
